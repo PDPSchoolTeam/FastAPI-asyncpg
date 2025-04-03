97030034ae735ed1bb6b1c24639c24716b7f1929
mrx <meibrohimov@gmail.com>
2025-04-03 12:24:53 +0500

    add test file

--- config.py
+++ config.py
@@ -9,4 +9,7 @@ DB_PASS = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("POSTGRES_HOST")
 DB_PORT = os.getenv("POSTGRES_PORT")
 
-DATABASE_URL = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://neondb_owner:npg_qczbo3IxvXy8@ep-nameless-thunder-a165i082-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+
+
